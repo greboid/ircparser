b55c0b46042deb33329109f90dbc7df7c400412c
Greg Holmes <git@greg.holmes.name>
2025-07-26 18:38:22 +0100

    Use flags for example program

--- example/example.go
+++ example/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/csmith/envflag/v2"
 	"github.com/csmith/slogflags"
@@ -10,6 +11,18 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+)
+
+var (
+	nickname     = flag.String("nickname", "", "")
+	username     = flag.String("username", "", "")
+	hostname     = flag.String("hostname", "", "")
+	port         = flag.Int("port", 6697, "")
+	tls          = flag.Bool("tls", true, "")
+	saslusername = flag.String("saslusername", "", "")
+	saslpassword = flag.String("saslpassword", "", "")
+	joinChannel  = flag.String("joinchannel", "", "")
 )
 
 func main() {
@@ -18,21 +31,36 @@ func main() {
 		slogflags.WithCustomLevels(map[string]slog.Level{"TRACE": parser.LogTrace}),
 		slogflags.WithSetDefault(true),
 	)
-	conf := parser.NewConnectionConfig("irc.quakenet.org", 6667, false)
-	conf.Nick = "Greparser"
-	conf.Username = "Greboid"
-	conf.SASLUser = ""
-	conf.SASLPass = ""
-	conf.TLS = false
+	if *nickname == "" || *hostname == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *username == "" {
+		*username = *nickname
+	}
+	conf := parser.NewConnectionConfig(*hostname, *port, *tls)
+	conf.Nick = *nickname
+	conf.Username = *username
+	conf.SASLUser = *saslusername
+	conf.SASLPass = *saslpassword
 	con := parser.NewParser(context.Background(), conf)
+	channels := 0
+	con.Subscribe(parser.EventJoin, func(event *parser.Event) {
+		if joinData, ok := event.Data.(*parser.JoinData); ok {
+			fmt.Println("Joined: " + joinData.Nick + " " + joinData.Channel)
+		}
+	})
+	go func() {
+		time.Sleep(5 * time.Second)
+		if channels == 0 && *joinChannel != "" {
+			con.Join(*joinChannel)
+		}
+	}()
 	err := con.Start()
 	if err != nil {
 		panic(err)
 	}
 	defer con.Stop()
-	con.Subscribe(parser.EventRegistered, func(event *parser.Event) {
-		con.Join("#noshame")
-	})
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
